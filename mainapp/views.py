0a03f19e546376cba824ff6ab4a051368d62402c
hakimjanov-07 <abdullohhakimjanov0721@gmail.com>
2026-04-16 12:18:44 +0500

    Pagination qo'shildi

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 
 from django.urls import path, include
 from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers, serializers, viewsets,pagination
 from .serializers import Application_Serializer, Category_Serializer, Worker_Serializer
 from .models import Application, Worker, Category
 
+"Pagination class "
+class our_pagination(pagination.PageNumberPagination):
+    page_size = 3
+    page_size_query_param ='page_size'
+    max_page_size = 10000
+    
 class ApplicationViewSet(viewsets.ModelViewSet):
     queryset = Application.objects.filter(status='qabul_qilingan')
     serializer_class = Application_Serializer
